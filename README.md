31d6f31b2bd3dae6cdfc6d53fd2ca6e0c9732507
Adam CHEN JINGHAO <jinghao_chen@hotmail.com>
2022-12-04 22:38:08 +0800

    wa_test8

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # devops_cicd_project_adamcjh
 This is NOT a "Homework" for DevOps class!
+
--- run.py
+++ run.py
@@ -1,73 +1,85 @@
 import os
 from twilio.rest import Client
 
-# Get the twilio client specific values from env
-ACCOUNT_SID = os.environ['ACCOUNT_SID']
-AUTH_TOKEN = os.environ['AUTH_TOKEN']
-TO_WHATSAPP_NUMBER  = os.environ['TO_WHATSAPP_NUMBER']
-FROM_WHATSAPP_NUMBER = "+14155238886"
+title = os.getenv("INPUT_IUTITLE")
+body = os.getenv("INPUT_IU_BODY")
+comment = os.getenv("INPUT_IU_COM")
+num = os.getenv("INPUT_IUNUM")
+name = os.getenv('GITHUB_ACTOR')
+event = os.getenv("GITHUB_EVENT_NAME")
+repo = os.getenv("GITHUB_REPOSITORY")
+action = os.getenv('INPUT_EVENT_ACTION')
+stars = os.getenv("INPUT_STARGAZERS")
+forks = os.getenv("INPUT_FORKERS")
+From = os.getenv('INPUT_FROM')
+To = os.getenv('INPUT_TO')
+account_sid = os.getenv('INPUT_TWILIO_ACCOUNT_SID')
+authtoken = os.getenv('INPUT_TWILIO_AUTH')
 
-# Get the github event specific values from env
-GITHUB_SERVER_URL = os.getenv("GITHUB_SERVER_URL")
-REPOSITORY = os.getenv("GITHUB_REPOSITORY")
-GITHUB_ACTOR = os.getenv("GITHUB_ACTOR")
-GITHUB_EVENT_NAME = os.getenv("GITHUB_EVENT_NAME")
-EVENT_NAME = os.getenv("GITHUB_EVENT_NAME")
-PR_NUMBER = os.getenv("INPUT_PR_NUMBER")
-PR_TITLE=os.getenv("INPUT_PR_TITLE")
-PR_BODY=os.getenv("INPUT_PR_BODY")
-ISSUE_TITLE  = os.getenv("INPUT_ISSUE_TITLE")
-ISSUE_NUMBER = os.getenv('INPUT_ISSUE_NUMBER')
-ISSUE_BODY = os.getenv('INPUT_ISSUE_BODY')
-REPO_FORK_COUNT = os.getenv('INPUT_REPO_FORK_COUNT')
-REPO_WATCH_COUNT = os.getenv('INPUT_REPO_WATCH_COUNT')
+if title is None:
+    title = os.getenv('INPUT_PRTITLE')
+if num is None:
+    num = os.getenv('INPUT_PRNUM')
+if body is None:
+    body = os.getenv("INPUT_PR_BODY")
 
-repo_url = f"{GITHUB_SERVER_URL}/{REPOSITORY}"
-response = f"Hello there, \n"
 
-# Process the event and prepare Whatsapp message payload
-media_url_for_avatar = [f"{GITHUB_SERVER_URL}/{GITHUB_ACTOR}.png"]
-if GITHUB_EVENT_NAME == "push":
-    response += f"There is a new *push* in your repository *{REPOSITORY}* by *{GITHUB_ACTOR}*.\n\n"
-    response += f"*Repository URL*: {repo_url}"
-elif GITHUB_EVENT_NAME == "pull_request":
-    pr_url = f"{repo_url}/pull/{PR_NUMBER}"
-    response += f"A new event was triggered for a *Pull Request* in your repository *{REPOSITORY}*.\n\n"
-    response += f"*PR Number*: ```#{PR_NUMBER}```\n"
-    response += f"*PR Title* : ```{PR_TITLE}```\n"
-    response += f"*PR User*  : ```{GITHUB_ACTOR}```\n"
-    response += f"*PR Body*  : _{PR_BODY}_\n\n"
-    response += f"*Check it out*: {pr_url}\n"
-    response += f"*Repository URL*: {repo_url}"
-elif GITHUB_EVENT_NAME == "issues":
-    issue_url = f"{repo_url}/issues/{ISSUE_NUMBER}"
-    response += f"A new event was triggered for an *Issue* in your repository *{REPOSITORY}*.\n\n"
-    response += f"*Issue Number*: ```#{ISSUE_NUMBER}```\n"
-    response += f"*Issue Title* : ```{ISSUE_TITLE}```\n"
-    response += f"*Issue User*  : ```{GITHUB_ACTOR}```\n"
-    response += f"*Issue Body*  : _{ISSUE_BODY}_\n\n"
-    response += f"*Check it out*: {issue_url}\n"
-    response += f"*Repository URL*: {repo_url}"
-elif GITHUB_EVENT_NAME == "fork":
-    response += f"Your repository *{REPOSITORY}* was forked by *{GITHUB_ACTOR}*.\n"
-    response += f"Current fork count: *{REPO_FORK_COUNT}*\n\n"
-    response += f"*Repository URL*: {repo_url}"
-elif GITHUB_EVENT_NAME == "watch":
-    response += f"Your repository *{REPOSITORY}* is now watched by *{GITHUB_ACTOR}*.\n"
-    response += f"Current watch count: *{REPO_WATCH_COUNT}*\n\n"
-    response += f"*Repository URL*: {repo_url}"
+# ---------- Checking for responses ---------------
+
+if event == "pull_request":
+    repo_url = f'https://github.com/{repo}/pulls/{num}'
+    if action == "opened":
+        response = f'A new Pull request *{title}* *#{num}* has been opened in *{repo}* by *{name}*  \n\n {repo_url}'
+
+    elif action == 'closed':
+        response = f'The Pull request *{title}* *#{num}* on *{repo}* has been closed by *{name}*, \n\n {repo_url}'
+
+    else:
+        response = f'A new {action} event was triggered on the PR *{title}* *#{num}* on *{repo}*  by *{name}*, \n\n {repo_url}'
+
+
+elif event == 'issues':
+    repo_url = f'https://github.com/{repo}/issues/{num}'
+    if action == 'opened':
+        response = f'A new Issue *{title}* *#{num}* has been opened in *{repo}* by *{name}* \n\n {repo_url}'
+
+    elif action == 'closed':
+        response = f'The Issue *{title}* *#{num}* on *{repo}* was closed by *{name}*, \n\n {repo_url}'
+
+    elif action == 'reopened':
+        response = f'The Issue *{title}* *#{num}* on *{repo}* was reopened by *{name}*, \n\n {repo_url}'
+
+    else:
+        response = f'A new {action}  on the issue *{title}* *#{num}* on *{repo}* was done  by *{name}*, \n\n {repo_url}'
+
+
+elif event == 'issue_comment':
+    repo_url = f'https://github.com/{repo}/issues/{num}'
+    response = f'*{name}* commented  _{comment}_ on issue *#{num}* of *{repo}*  \n\n {repo_url}'
+
+elif event == 'page_build':
+    page_url = 'https://'+name+'.github.io/'+repo
+    response = f'A new page build was triggered on the  on *{repo}*  by *{name}*, \n\n {page_url}'
+
+elif event == 'watch':
+    repo_url = f'https://github.com/{repo}/'
+    response = f'*{name}* starred/forked your repo *{repo}* \n *Stars:{stars}* \n *Forks:{forks}* \n\n{repo_url}'
+
+
 else:
-    response += f"A new *{GITHUB_EVENT_NAME}* event was triggered by *{GITHUB_ACTOR}* in your repository *{REPOSITORY}*\n\n"
-    response += f"*Repository URL*: {repo_url}"
+    repo_url = f'https://github.com/{repo}'
+    response = f'A new *{event}* has been made in *{repo}* by *{name}*, \n\n {repo_url}'
 
-# Create twilio client from the given authentication token and account sid
-client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
-# Prepare and send the message payload
-message = client.messages.create(
-                              media_url=media_url_for_avatar if media_url_for_avatar else None,
-                              body=response,
-                              to=f"whatsapp:{TO_WHATSAPP_NUMBER}"
-                          )
+#creating twilio client
 
-print(f"Whatsapp Message ID: {message.sid}")
+From = 'whatsapp:'+From
+To = 'whatsapp:'+To
+client = Client()
+message = client.messages \
+    .create(
+        from_=From,
+        body=response,
+        to=To
+    )
+print('Message Sent')
