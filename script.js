6f9c9c0465ebe9eb3981a4b04705e113cc871976
Adam CHEN JINGHAO <jinghao_chen@hotmail.com>
2022-12-04 23:29:42 +0800

    handsign_change

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
 function highFive(x){
-    document.getElementById("highFive").classList = "fa-solid fa-hand fa-4x";
+    document.getElementById("highFive").classList = "fa-solid fa-hand-spock fa-4x";
+    document.getElementById("livelongandprosper").textContent = "Live Long And Prosper"
 }
